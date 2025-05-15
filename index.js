c2eeef99c0dc51926f6966fbf251045ff6f9dbad
Aravind-R-Warrier <aravindrwarrier1@gmail.com>
2025-05-15 22:20:30 +0530

    feedback-review-sytem

--- index.js
+++ index.js
@@ -36,7 +36,9 @@ const offerRouter = require("./routes/offerRoutes");
 
 const orderRouter = require("./routes/orderRoutes");
 
-const couponRoutes = require("./routes/couponRoutes"); 
+const couponRoutes = require("./routes/couponRoutes");
+
+const feedbackRoutes = require("./routes/feedbackRoutes"); 
 
 
 
@@ -55,11 +57,23 @@ app.use("/restaurants",offerRouter)
 app.use("/order",orderRouter)
 app.use("/coupon",couponRoutes)
 
+// {
+//   "userId": "6825a5545d887cc953bd60c0",         
+//   "orderId": "6825a7199f3329682fd6294b",
+//   "restaurantId": "6822e6600cd0390135d35483",
+//   "agentId": "AGENT_OBJECT_ID",
+//   "targetType": "restaurant",
+//   "rating": 5,
+//   "comment": "Fantastic food!"
+// }
 
 
 app.use("/resturants",resturantRouter)
 app.use("/location",locationRouter)
 app.use("/agent",agentRouter)
+
+
+app.use("/feedback",feedbackRoutes)
 
 
 
