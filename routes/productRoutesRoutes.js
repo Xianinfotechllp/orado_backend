1d57cdbf7bc24b6b836e1f07e9a78b8668ab4043
Aravind <aravindrwarrier1@gmail.com>
2025-06-08 02:51:53 -0700

    --

--- routes/productRoutesRoutes.js
+++ routes/productRoutesRoutes.js
@@ -6,8 +6,8 @@ const router = express.Router();
 
 router.post('/:restaurantId/products', protect, checkRole('merchant'), upload.array('images'), createProduct);
 router.get('/:restaurantId/products', protect, checkRole('merchant', 'customer'), getRestaurantProducts);
-router.put('/products/:productId', protect, checkRole('merchant'),  upload.array('images'), updateProduct);
-router.delete('/products/:productId', protect, checkRole('merchant'), deleteProduct);
+router.put('/products/:productId', protect, checkRole('merchant','superAdmin'),  upload.array('images'), updateProduct);
+router.delete('/products/:productId', protect, checkRole('merchant','superAdmin'), deleteProduct);
 router.put('/products/:productId/auto-on-off', protect, checkRole('merchant'), toggleProductActive);
 
 router.get("/:restaurantId/products/category/:categoryId",getCategoryProducts)
