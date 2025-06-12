b78331a8b5e7b995cac62fce7ee037d7408ecf63
amarnadh777 <amarnadh6565@gmail.com>
2025-06-12 23:13:29 +0530

    cors fix

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
     origin: ["http://localhost:5173", "https://orado.work.gd"], // exact origin of your frontend
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST","PUT","DELETE","OPTIONS"],
     credentials: true,
   },
 });
