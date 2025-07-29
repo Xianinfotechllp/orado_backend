772173f7b72f8e98c37a94e389b874a83201ed22
amarnadh777 <amarnadh6565@gmail.com>
2025-07-30 00:56:36 +0530

    format date

--- src/api/flutter/controllers/userControllers.js
+++ src/api/flutter/controllers/userControllers.js
@@ -1445,9 +1445,7 @@ function formatDate(date) {
   return new Date(date).toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'long',
-    day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit'
+    day: 'numeric'
   });
 }
 exports.getPromoCodesForCustomerAndRestaurant = async (req, res) => {
